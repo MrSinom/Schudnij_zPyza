8cd0c14f26114a6a26c4c38184dffb58b63d840d
Szymon <gebala.simon@gmail.com>
2025-10-22 13:59:27 +0200

    Wprowadzone zmiany przed deployem

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Schudnij_zPyza/">
       <App />
     </BrowserRouter>
   </StrictMode>
